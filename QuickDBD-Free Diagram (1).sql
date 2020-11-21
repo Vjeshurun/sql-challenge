d1a2804c27b114d52b1eeab3d1b5da4977fae1d3
Vjeshurun <liu.victoria01@gmail.com>
2020-11-20 22:08:53 -0600

    adding additional doc

---
+++ QuickDBD-Free Diagram (1).sql
@@ -0,0 +1,99 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/yt1YAe
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "departments" (
+    "dept_no" Varchar   NOT NULL,
+    "dept_name" Varchar   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" Varchar   NOT NULL,
+    "birth_date" Date   NOT NULL,
+    "first_name" Varchar   NOT NULL,
+    "last_name" Varchar   NOT NULL,
+    "sex" Varchar   NOT NULL,
+    "hire_date" Date   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" Varchar   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" Varchar   NOT NULL,
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "titles" (
+    "title_id" Varchar   NOT NULL,
+    "title" Varchar   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+select * from employees
+
+ --1.List the following details of each employee: employee number, last name, first name, sex, and salary.
+ Select e.emp_no as Employee_Number, e.last_name as Last_Name, e.first_name as First_Name,e.sex, s.salary 
+ from employees as e join salaries as s on (e.emp_no = s.emp_no)
+
+--Select * from salaries
+2. List first name, last name, and hire date for employees who were hired in 1986.
+Select first_name, last_name, hire_date 
+from employees 
+where EXTRACT(YEAR FROM hire_date) in (1986)
+
+3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+Select 
+4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
